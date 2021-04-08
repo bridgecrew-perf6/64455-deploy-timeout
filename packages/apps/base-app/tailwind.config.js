9dfe8c653ef3b7a062bb64f332d32f992dba090a
Fabien Franzen <info@atelierfabien.be>
2021-04-08 11:34:37 +0200

    Make sure local node_modules is handled by Tailwind

--- packages/apps/base-app/tailwind.config.js
+++ packages/apps/base-app/tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   purge: [
     './public/**/*.html',
     './src/**/*.{js,jsx,ts,tsx}',
+    './node_modules/@atelierfabien/mono-next/**/*.{js,jsx,ts,tsx}',
     '../../../node_modules/@atelierfabien/mono-next/**/*.{js,jsx,ts,tsx}'
   ],
   darkMode: false, // or 'media' or 'class'
