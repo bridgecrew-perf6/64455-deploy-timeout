9087095a8b9f5106738e1973f3b53d78850f3d63
Fabien Franzen <info@atelierfabien.be>
2021-04-08 12:09:53 +0200

    Resolve mono-next path for Tailwind config

--- packages/apps/base-app/tailwind.config.js
+++ packages/apps/base-app/tailwind.config.js
@@ -1,9 +1,12 @@
+const { dirname } = require('path');
+
+const mono = dirname(require.resolve('@atelierfabien/mono-next'));
+
 module.exports = {
   purge: [
     './public/**/*.html',
     './src/**/*.{js,jsx,ts,tsx}',
-    './node_modules/@atelierfabien/mono-next/**/*.{js,jsx,ts,tsx}',
-    '../../../node_modules/@atelierfabien/mono-next/**/*.{js,jsx,ts,tsx}'
+    `${mono}/**/*.{js,jsx,ts,tsx}`
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
