33f7113336cea762fc0d50f32502f210069d4974
Fabien Franzen <info@atelierfabien.be>
2021-04-10 13:50:46 +0200

    Remove Link.defaults from _app

--- packages/apps/base-app/src/pages/_app.js
+++ packages/apps/base-app/src/pages/_app.js
@@ -1,9 +1,4 @@
 import App from '@mono/components/App';
-import { Link } from '@mono/next';
-
-// Link settings
-Link.defaults.activeClassName = 'uk-active';
-Link.defaults.matchClassName = 'uk-active-match';
 
 // Global scss
 import '@app/styles/uikit.scss';
