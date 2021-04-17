94ee8e622bb0ae98309a6ab67ce4fc6de453ed36
Fabien Franzen <info@atelierfabien.be>
2021-04-17 16:19:29 +0200

    Properly merge Tailwind purge option

--- packages/modules/next-foundation/config/tailwind.js
+++ packages/modules/next-foundation/config/tailwind.js
@@ -3,13 +3,14 @@ const path = require('path');
 const local = path.resolve(path.join(__dirname, '..'));
 
 module.exports = (config = {}) => {
+  const { purge, ...overrides } = config;
   return {
     purge: [
       './public/**/*.html',
       './src/**/*.{js,jsx,ts,tsx}',
       './slices/**/*.{js,jsx,ts,tsx}',
       `${local}/**/*.{js,jsx,ts,tsx}`,
-      ...(config.purge || []),
+      ...(purge || []),
     ],
     darkMode: false, // or 'media' or 'class'
     theme: {
@@ -23,6 +24,6 @@ module.exports = (config = {}) => {
         },
       },
     },
-    ...config,
+    ...overrides,
   };
 };
