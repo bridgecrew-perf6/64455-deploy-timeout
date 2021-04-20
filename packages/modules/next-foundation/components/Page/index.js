791da040c737c837014a82ff5e91877beb3f3edb
Fabien Franzen <info@atelierfabien.be>
2021-04-20 16:17:11 +0200

    Use pick() utility to extract SEO props

--- packages/modules/next-foundation/components/Page/index.js
+++ packages/modules/next-foundation/components/Page/index.js
@@ -1,6 +1,25 @@
 import { PageSeo } from '../../lib';
+import { pick } from '../../lib/util';
 
-const Page = ({ children, ...seo }) => {
+const SEO_PROPS = [
+  'title',
+  'noindex',
+  'nofollow',
+  'robotsProps',
+  'description',
+  'canonical',
+  'openGraph',
+  'facebook',
+  'twitter',
+  'additionalMetaTags',
+  'titleTemplate',
+  'mobileAlternate',
+  'languageAlternates',
+  'additionalLinkTags',
+];
+
+const Page = ({ children, ...props }) => {
+  const seo = pick(props, SEO_PROPS);
   return (
     <>
       <PageSeo {...seo} />
