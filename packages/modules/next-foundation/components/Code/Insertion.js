ac390726452ff03128153ff5272ad61c78d4c9c7
Fabien Franzen <info@atelierfabien.be>
2021-06-09 15:35:57 +0200

    Add Code component

---
+++ packages/modules/next-foundation/components/Code/Insertion.js
@@ -0,0 +1,29 @@
+/* eslint-disable react/no-danger */
+
+import Head from 'next/head';
+import { isBlank } from '../../lib/util';
+
+const CodeInsertion = ({ id, source }) => {
+  const { language, code } = source;
+  const hasSource = !isBlank(code);
+  if (language === 'html' && hasSource) {
+    return (
+      <div className="tm-expand" dangerouslySetInnerHTML={{ __html: code }} />
+    );
+  } else if (language === 'css' && hasSource) {
+    return (
+      <Head key={id}>
+        <style dangerouslySetInnerHTML={{ __html: code }} />
+      </Head>
+    );
+  } else if (language === 'js' && hasSource) {
+    return (
+      <Head key={id}>
+        <script dangerouslySetInnerHTML={{ __html: code }} />
+      </Head>
+    );
+  }
+  return null;
+};
+
+export default CodeInsertion;
