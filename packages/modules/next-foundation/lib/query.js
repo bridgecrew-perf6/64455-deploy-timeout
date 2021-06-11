2bfbdd173bede1a52dfc396efb668bff115aa4a3
Fabien Franzen <info@atelierfabien.be>
2021-06-11 19:00:18 +0200

    Minor tweaks

--- packages/modules/next-foundation/lib/query.js
+++ packages/modules/next-foundation/lib/query.js
@@ -1,8 +1,10 @@
 export * from 'react-query';
 
-export const wrapQuery = fn => {
+export const wrapQuery = (fn, offset = false) => {
   return ({ queryKey }) => {
-    const [, ...args] = queryKey;
+    const args = queryKey.slice(
+      typeof offset === 'number' ? offset : offset === true ? 1 : 0
+    );
     return fn(...args);
   };
 };
--- packages/modules/next-foundation/lib/translation.js
+++ packages/modules/next-foundation/lib/translation.js
@@ -44,18 +44,20 @@ export function useTranslator(forceLocale) {
   const defaultLocale = router.defaultLocale ?? 'en';
   const locale = forceLocale ?? router.locale ?? defaultLocale;
 
-  return (data, ...path) => {
-    const key = path
-      .map(p => (Array.isArray(p) ? p : String(p).split('.')))
-      .flat();
-    const defaultKey = [].concat(key);
-    defaultKey.splice(-1, 0, 'i18n', defaultLocale);
-    const localizedKey = [].concat(key);
-    localizedKey.splice(-1, 0, 'i18n', locale);
-    return get(
-      data,
-      localizedKey,
-      get(data, defaultKey, key.length > 0 ? get(data, key) : data)
-    );
-  };
+  return useMemo(() => {
+    return (data, ...path) => {
+      const key = path
+        .map(p => (Array.isArray(p) ? p : String(p).split('.')))
+        .flat();
+      const defaultKey = [].concat(key);
+      defaultKey.splice(-1, 0, 'i18n', defaultLocale);
+      const localizedKey = [].concat(key);
+      localizedKey.splice(-1, 0, 'i18n', locale);
+      return get(
+        data,
+        localizedKey,
+        get(data, defaultKey, key.length > 0 ? get(data, key) : data)
+      );
+    };
+  }, [defaultLocale, locale]);
 }
