b0be3c8e9a3a8d5f3b9c5c32745cc84bcf8b93f5
Fabien Franzen <info@atelierfabien.be>
2021-06-24 19:40:09 +0200

    Rework variant pills

--- packages/modules/next-shop/components/Product/Variants/Variant/Color.js
+++ packages/modules/next-shop/components/Product/Variants/Variant/Color.js
@@ -1,26 +1,30 @@
 const ProductVariantColor = ({ title }) => (
   <div>
     <div className="uk-text-small uk-margin-xsmall-bottom">{title}</div>
-    <ul className="uk-subnav uk-subnav-pill tm-variations" uk-switcher="true">
-      <li>
-        <a className="tm-variation-color" uk-tooltip="Space Grey">
+    <div
+      className="uk-grid uk-grid-xsmall tm-variations"
+      uk-grid="true"
+      uk-switcher="true"
+    >
+      <div>
+        <a className="tm-pill tm-variation-color" uk-tooltip="Space Grey">
           <div
             style={{
               backgroundColor: '#aaaeb1',
             }}
           />
         </a>
-      </li>
-      <li>
-        <a className="tm-variation-color" uk-tooltip="Silver">
+      </div>
+      <div>
+        <a className="tm-pill tm-variation-color" uk-tooltip="Silver">
           <div
             style={{
               backgroundColor: '#dddfde',
             }}
           />
         </a>
-      </li>
-    </ul>
+      </div>
+    </div>
   </div>
 );
 
--- packages/modules/next-shop/components/Product/Variants/Variant/Value.js
+++ packages/modules/next-shop/components/Product/Variants/Variant/Value.js
@@ -1,14 +1,22 @@
 const ProductVariantValue = ({ title }) => (
   <div>
     <div className="uk-text-small uk-margin-xsmall-bottom">{title}</div>
-    <ul className="uk-subnav uk-subnav-pill tm-variations" uk-switcher="">
-      <li aria-expanded="true" className="uk-active">
-        <a>256 GB</a>
-      </li>
-      <li aria-expanded="false">
-        <a>512 GB</a>
-      </li>
-    </ul>
+    <div
+      className="uk-grid uk-grid-xsmall tm-variations"
+      uk-grid="true"
+      uk-switcher="true"
+    >
+      <div>
+        <div className="tm-pill tm-variation-value">
+          <a>256 GB</a>
+        </div>
+      </div>
+      <div>
+        <div className="tm-pill tm-variation-value">
+          <a>512 GB</a>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
