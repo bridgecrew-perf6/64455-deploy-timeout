29ceb63e7b52f7282debc3394638da314613c660
Fabien Franzen <info@atelierfabien.be>
2021-12-18 20:15:07 +0100

    Refactor page and node handling for pages

---
+++ packages/modules/next-base/src/http/nodes.js
@@ -0,0 +1,78 @@
+/* eslint-disable prettier/prettier */
+
+import { get, trim } from '@atelierfabien/next-foundation/lib/util';
+
+import { getClient } from '@atelierfabien/next-sanity/lib/server';
+
+import {
+  getClient as getBrowserClient,
+  usePreviewQueryProps,
+} from '@atelierfabien/next-sanity';
+
+import init from '@app/sanity/types/node';
+
+import Container from '@shop/components/Page/Container';
+
+import config from '@app/config/app';
+
+import { defaultLocale } from '@root/i18n';
+
+const nodes = init(getBrowserClient());
+
+const homepageId = config?.homepage?.page;
+
+export const getPagePropsByPath = async (path, options = {}) => {
+  const { preview = false } = options;
+  const segments = [].concat(
+    Array.isArray(path) ? path : trim(path.split('/'), '/')
+  );
+
+  const nodes = init(getClient(preview));
+
+  const node = await nodes.getByPath(segments, {
+    locale: options.locale,
+  });
+
+  if (node && node.item?._type === 'page') {
+    const props = await nodes.resolveProps(node.item, {
+      ...options,
+      node,
+    });
+    if (preview) {
+      props.previewOptions = {
+        studioUrl: `/desk/site;${node.item._type};${node.item._id}`,
+        options,
+      };
+    }
+    return { props };
+  } else {
+    return {
+      notFound: true,
+    };
+  }
+};
+
+export const getStaticProps = async (context) => {
+  return getPagePropsByPath(context.params?.path, context);
+};
+
+export const getServerProps = async (context) => {
+  return getPagePropsByPath(context.params?.path, context);
+};
+
+export const getStaticPaths = async (context) => {
+  const nodes = init(getClient());
+  const paths = await nodes.getStaticPaths(context, (doc) => {
+    const path = get(doc, ['i18n', defaultLocale, 'path']);
+    return doc.item?._ref !== homepageId && path !== '/';
+  });
+  return { paths, fallback: false };
+};
+
+export const prepareData = (node, _props, context) =>
+  nodes.resolveProps(node.item, { ...context, node });
+
+export const Page = (props) => {
+  const pageProps = usePreviewQueryProps(props, { fn: prepareData });
+  return <Container {...pageProps} />;
+};
---
+++ packages/modules/next-base/src/http/pages.js
@@ -0,0 +1,72 @@
+import { getClient } from '@atelierfabien/next-sanity/lib/server';
+
+import {
+  getClient as getBrowserClient,
+  usePreviewQueryProps,
+} from '@atelierfabien/next-sanity';
+
+import Container from '@shop/components/Page/Container';
+
+import init from '@app/sanity/types/page';
+
+import config from '@app/config/app';
+
+const homepageId = config?.homepage?.page;
+
+const pages = init(getBrowserClient());
+
+export const getPagePropsByAlias = async (alias, options = {}) => {
+  const { preview = false } = options;
+
+  const previewOptions = preview ? {} : undefined;
+
+  const pages = init(getClient(preview));
+
+  const item = await pages.getByAlias(
+    alias,
+    { locale: options.locale },
+    previewOptions
+  );
+
+  if (item?._type === 'page') {
+    const props = await pages.resolveProps(item, { ...options });
+    if (preview) {
+      props.previewOptions = {
+        studioUrl: `/desk/site;${item._type};${item._id}`,
+        ...previewOptions,
+        options,
+      };
+    }
+    return { props };
+  } else {
+    return {
+      notFound: true,
+    };
+  }
+};
+
+export const getStaticProps = async context => {
+  const path = context.params?.path;
+  return getPagePropsByAlias(Array.isArray(path) ? path[0] : path, context);
+};
+
+export const getServerProps = async context => {
+  const path = context.params?.path;
+  return getPagePropsByAlias(Array.isArray(path) ? path[0] : path, context);
+};
+
+export const getStaticPaths = async context => {
+  const paths = await pages.getStaticPaths(context, doc => {
+    return homepageId !== doc._id;
+  });
+
+  return { paths, fallback: false };
+};
+
+export const prepareData = (item, _props, context) =>
+  pages.resolveProps(item, { ...context });
+
+export const Page = props => {
+  const pageProps = usePreviewQueryProps(props, { fn: prepareData });
+  return <Container {...pageProps} />;
+};
