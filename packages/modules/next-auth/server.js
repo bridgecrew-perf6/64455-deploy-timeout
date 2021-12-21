f3ea613370bda518936723f66a12d628d820262e
Fabien Franzen <info@atelierfabien.be>
2021-12-21 14:55:06 +0100

    Make configure function standalone

--- packages/modules/next-auth/server.js
+++ packages/modules/next-auth/server.js
@@ -1,7 +1,2 @@
-import configure from './src/lib/configure';
-import * as queries from './src/lib/queries';
-
 export * from './src/lib/email';
 export * from './src/lib/handlers';
-
-export { configure, queries };
