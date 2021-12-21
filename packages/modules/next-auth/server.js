8d90d1670af6e0f591bda013e4d948cf8a65cc3c
Fabien Franzen <info@atelierfabien.be>
2021-12-21 14:12:40 +0100

    Add next-auth/auth for re-exported core dependencies

--- packages/modules/next-auth/server.js
+++ packages/modules/next-auth/server.js
@@ -1,5 +1,3 @@
-import NextAuth from 'next-auth';
-import Providers from 'next-auth/providers';
 import configure from './src/lib/configure';
 import SanityCredentials from './src/lib/credentials';
 import SanityAdapter from './src/lib/adapter';
@@ -8,11 +6,4 @@ import * as queries from './src/lib/queries';
 export * from './src/lib/email';
 export * from './src/lib/handlers';
 
-export {
-  NextAuth,
-  Providers,
-  SanityCredentials,
-  SanityAdapter,
-  configure,
-  queries,
-};
+export { SanityCredentials, SanityAdapter, configure, queries };
