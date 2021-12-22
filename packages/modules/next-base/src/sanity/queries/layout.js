8033c2a103e544d960dd8ae9711806d5db327e9d
Fabien Franzen <info@atelierfabien.be>
2021-12-22 15:20:37 +0100

    Temporarily change layoutProjection

--- packages/modules/next-base/src/sanity/queries/layout.js
+++ packages/modules/next-base/src/sanity/queries/layout.js
@@ -1,14 +1,20 @@
 import groq from 'groq';
 
-import { buildNodesProjection } from '@app/lib/node';
+// import { buildNodesProjection } from '@app/lib/node';
 
-import { assetsProjection, fragmentsProjection, sectionsProjection } from '.';
+import { assetsProjection } from '.';
+
+// export const layoutProjection = groq`
+//   ...,
+//   'identifier': alias.current,
+//   'assets': ${assetsProjection},
+//   'fragments': ${fragmentsProjection},
+//   'sections': ${sectionsProjection},
+//   'navigation': navigation->{ ${buildNodesProjection(1)} }
+// `;
 
 export const layoutProjection = groq`
   ...,
   'identifier': alias.current,
-  'assets': ${assetsProjection},
-  'fragments': ${fragmentsProjection},
-  'sections': ${sectionsProjection},
-  'navigation': navigation->{ ${buildNodesProjection(1)} }
+  'assets': ${assetsProjection}
 `;
