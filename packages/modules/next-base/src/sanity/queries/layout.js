d2e09a6d32da86669ffc7805249cfa6b79cd919a
Fabien Franzen <info@atelierfabien.be>
2021-12-22 15:53:24 +0100

    Release v.0.0.31

--- packages/modules/next-base/src/sanity/queries/layout.js
+++ packages/modules/next-base/src/sanity/queries/layout.js
@@ -1,20 +1,14 @@
 import groq from 'groq';
 
-// import { buildNodesProjection } from '@app/lib/node';
+import { buildNodesProjection } from '@app/lib/node';
 
-import { assetsProjection } from '.';
-
-// export const layoutProjection = groq`
-//   ...,
-//   'identifier': alias.current,
-//   'assets': ${assetsProjection},
-//   'fragments': ${fragmentsProjection},
-//   'sections': ${sectionsProjection},
-//   'navigation': navigation->{ ${buildNodesProjection(1)} }
-// `;
+import { assetsProjection, fragmentsProjection, sectionsProjection } from '.';
 
 export const layoutProjection = groq`
   ...,
   'identifier': alias.current,
-  'assets': ${assetsProjection}
+  'assets': ${assetsProjection},
+  'fragments': ${fragmentsProjection},
+  'sections': ${sectionsProjection},
+  'navigation': navigation->{ ${buildNodesProjection(1)} }
 `;
